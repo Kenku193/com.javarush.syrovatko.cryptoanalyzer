0e7e7b16e2fcece1797518b98b5dc9a4c53cab6a
mentor <kenku@mail.ru>
2024-10-29 20:57:42 +0300

    Initial commit

---
+++ src/Application.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Application {
+    public static void main(String[] args) {
+// Логика меню
+        // 1. Шифрование
+        // 2. Расшифровка с ключом
+        // 3. Brute force
+        // 4. Статистический анализ
+        // 0. Выход
+
+        System.out.println("ШИФРОВАЛЬЩИК МЕТОДОМ ЦЕЗАРЯ");
+        System.out.println("ВЫБЕРИТЕ ПУНКТ МЕНЮ");
+        System.out.println("1. Шифрование с ключом");
+        System.out.println("2. Расшифровка с ключом");
+        System.out.println("3. Brute force");
+        System.out.println("0. Выход");
+
+        Scanner console = new Scanner(System.in);
+        int j = console.nextInt();
+
+
+    }
+}
